a06f1785706095566cff180fb7d6d06fb4bd9d59
seagetch <sigetch@gmail.com>
2020-10-30 09:58:49 +0900

    Introduced '<<' and '>>' operator for context.

--- utflow/taskflow.py
+++ utflow/taskflow.py
@@ -2,6 +2,7 @@ import contextlib
 import os
 import pickle
 import uuid
+import inspect
 from logging import getLogger
 
 logger = getLogger(__name__)
@@ -29,8 +30,45 @@ class TaskFlow:
     class Task:
 
         class Context(object):
-            pass
-
+            
+            
+            def __lshift__(self, obj):
+                if isinstance(obj, dict):
+                    for k,v in obj.items():
+                        self.__dict__[str(k)] = v
+                elif isinstance(obj, type(self)):
+                    for k,v in obj.__dict__.items():
+                        self.__dict__[str(k)] = v
+                else:
+                    self._ = obj
+                return self
+            
+            
+            def __rshift__(self, obj):
+                if callable(obj):
+                    params = inspect.signature(obj).parameters
+                    args = self._ if "_" in self.__dict__ and isinstance(self._, list) else []
+                    kwargs = {}
+                    for i, (name, p) in enumerate(params.items()):
+                        if name in self.__dict__:
+                            kwargs[name] = self.__dict__[name]
+                        elif p.default is inspect.Parameter.empty and i >= len(args):
+                            raise Exception("parameter '%s' doesn't exist in %s"%(name, self))
+                    response = type(self)()
+                    response << obj(*args, **kwargs)
+                    return response
+                
+                elif isinstance(obj, type(self)):
+                    obj << self
+                    return obj
+                
+                elif isinstance(obj, dict):
+                    for k,v in self.__dict__.items():
+                        obj[str(k)] = v
+                    return obj
+                
+                return None
+            
         
         def __init__(self, flow, label, when=None):
             self.flow  = flow
