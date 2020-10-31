3101a6e8326c4d7f576fb5395ec447c9e727d285
seagetch <sigetch@gmail.com>
2020-10-31 20:22:57 +0900

    Introduced TaskFlow.start_next:
    start_next() enabled definition of task as function with decoration.

--- utflow/taskflow.py
+++ utflow/taskflow.py
@@ -10,6 +10,7 @@ logger = getLogger(__name__)
 class TaskFlow:
 
     def __init__(self, *, start=None, end=None):
+
         self.start     = start
         self.end       = end
         self.started   = False
@@ -18,25 +19,40 @@ class TaskFlow:
         self.context   = None
         self.last_task = None
 
-        
+
     @contextlib.contextmanager
     def next(self, label = None, when = None):
-        
+
         try:
             if label is None:
                 label = self.tasks
                 self.tasks += 1
             yield Task(self, label=label, when=when)
-            
+
         except Skip as e:
             logger.debug(" %s"%str(e))
             pass
-        
-        
+
+
+    def start_next(self, when = None):
+        def decorator(func):
+            label = func.__name__
+            task = Task(self, label=label, when=when)
+            try:
+                with task.start() as _:
+                    return func(_)
+            except Skip as e:
+                logger.debug(" %s"%str(e))
+
+        return decorator
+
+
     def reset(self, start = None, end = None):
-        
+
         self.start     = start
         self.end       = end
         self.started   = False
         self.ended     = False
+        self.tasks     = 0
+        self.context   = None
         self.last_task = None
